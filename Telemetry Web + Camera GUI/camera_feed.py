25af0008ebf9bf34d09c75e539d2254926ce16cb
maximum138 <maxwellyhchen@gmail.com>
2024-11-21 12:35:07 -0800

    added a camera stream with no gui

--- Telemetry Web + Camera GUI/camera_feed.py
+++ Telemetry Web + Camera GUI/camera_feed.py
@@ -3,10 +3,13 @@ import time
 import numpy as np
 from mjpeg_streamer import MjpegServer, Stream
 
-stream = Stream("camera", quality = 50, fps = 30) # size = (1920, 1080) is optional
+stream_gui = Stream("gui", quality = 50, fps = 30) # size = (1920, 1080) is optional
+stream_no_gui = Stream("no_gui", quality = 50, fps = 30)
 
-server = MjpegServer("10.3.141.1", 8080)
-server.add_stream(stream)
+# server = MjpegServer("10.3.141.1", 8080)
+server = MjpegServer("localhost", 8080)
+server.add_stream(stream_gui)
+server.add_stream(stream_no_gui)
 server.start()
 
 def AddImage(frame, image_src, x_pos, y_pos, scale):
@@ -29,15 +32,18 @@ def AddImage(frame, image_src, x_pos, y_pos, scale):
     return frame
 
 def GetCameraFrame():
-    # capture = cv.VideoCapture('http://192.168.137.216:8081')
-    capture = cv.VideoCapture(0, cv.CAP_V4L2) # Solution: https://stackoverflow.com/questions/77190490/usb-camera-doesnt-work-with-opencv-and-raspberry-pi
+    capture = cv.VideoCapture(0, cv.CAP_DSHOW)
+    # capture = cv.VideoCapture(0, cv.CAP_V4L2) # Solution: https://stackoverflow.com/questions/77190490/usb-camera-doesnt-work-with-opencv-and-raspberry-pi
 
     while True:
         isTrue, frame = capture.read()
 
-        #frame = AddImage(frame, "FRUCDHeader.png", 0, 0, 0.3)
+        stream_no_gui.set_frame(frame)
 
-        cv.putText(frame, str(time.time()), (255, 255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 0, 0), 2)
+        gui_frame = np.copy(frame)
+        gui_frame = AddImage(gui_frame, "FRUCDHeader.png", 0, 0, 0.3)
+
+        cv.putText(gui_frame, str(time.time()), (255, 255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 0, 0), 2)
 
         if __name__ == "__main__":
             #cv.imshow('Camera', frame)
@@ -45,7 +51,7 @@ def GetCameraFrame():
             if cv.waitKey(20) == ord(' '):
                 break
 
-        stream.set_frame(frame)
+        stream_gui.set_frame(gui_frame)
 
     server.stop()
     capture.release()
--- Telemetry Web + Camera GUI/telemetry_web_gui.py
+++ Telemetry Web + Camera GUI/telemetry_web_gui.py
@@ -2,8 +2,10 @@ import justpy as jp
 
 def ShowStream():
     wp = jp.WebPage()
-    img = jp.Img(src='http://10.3.141.1:8080/camera', a=wp)
+    # img = jp.Img(src='http://10.3.141.1:8080/camera', a=wp)
+    img = jp.Img(src='http://localhost:8080/camera', a=wp)
 
     return wp
 
-jp.justpy(ShowStream, host='10.3.141.1')
+#jp.justpy(ShowStream, host='10.3.141.1')
+jp.justpy(ShowStream)
