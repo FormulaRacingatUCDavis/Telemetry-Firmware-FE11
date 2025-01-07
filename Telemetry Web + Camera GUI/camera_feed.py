9dfa7ab7e4bbf73c01c590942277848d13bbb638
maximum138 <maxwellyhchen@gmail.com>
2025-01-07 15:40:22 -0800

    Changed to NiceGUI, more user friendly interface

--- Telemetry Web + Camera GUI/camera_feed.py
+++ Telemetry Web + Camera GUI/camera_feed.py
@@ -2,7 +2,6 @@ import cv2 as cv
 import time
 import numpy as np
 from mjpeg_streamer import MjpegServer, Stream
-import math
 
 STREAM_QUALITY = 50
 STREAM_FPS = 60
@@ -15,30 +14,38 @@ server.add_stream(stream_gui)
 server.add_stream(stream_no_gui)
 server.start()
 
-def AddImage(frame, image_src, x_pos, y_pos, scale):
-    logo = cv.imread(image_src)
+def AddImage(frame, image_src, x_pos, y_pos, scale = 1):
+    image = cv.imread(image_src)
 
-    logo_height = int(logo.shape[0] * scale)
-    logo_width = int(logo.shape[1] * scale)
+    image_height = int(image.shape[0] * scale)
+    image_width = int(image.shape[1] * scale)
 
-    logo = cv.resize(logo, dsize=(logo_width, logo_height), interpolation=cv.INTER_AREA)
+    image = cv.resize(image, dsize=(image_width, image_height), interpolation=cv.INTER_AREA)
 
-    logo_gray = cv.cvtColor(logo, cv.COLOR_BGR2GRAY)
-    ret, logo_mask = cv.threshold(logo_gray, 1, 255, type=cv.THRESH_BINARY)
+    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    ret, image_mask = cv.threshold(image_gray, 1, 255, type=cv.THRESH_BINARY)
 
-    logo_loc = frame[x_pos:logo_height, y_pos:logo_width]
+    image_loc = frame[x_pos:image_height+x_pos, y_pos:image_width+x_pos]
 
-    logo_loc[np.where(logo_mask)] = 0
+    image_loc[np.where(image_mask)] = 0
 
-    logo_loc += logo
+    image_loc += image
+
+    return frame
+
+def AddSpeedometer(frame, x_pos, y_pos, scale = 1):
+    normalized_value = ((frame[0,0,0] - 0) / (255 - 0)) * (360 - 180) + 180 # for speedometer testing
+    cv.ellipse(frame, (x_pos,y_pos), (100,100), 0, 180, normalized_value, (0,183,255), 10)
+    cv.putText(frame, str(frame[0,0,0]), (x_pos - 25,y_pos-25), cv.FONT_HERSHEY_DUPLEX, scale, (0,183,233), 2)
 
     return frame
 
 def GetCameraFrame():
-    capture = cv.VideoCapture(2, cv.CAP_ANY)
+    capture = cv.VideoCapture(0, cv.CAP_DSHOW)
     # capture = cv.VideoCapture(0, cv.CAP_V4L2) # Solution: https://stackoverflow.com/questions/77190490/usb-camera-doesnt-work-with-opencv-and-raspberry-pi
     capture.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
     capture.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
+    capture.set(cv.CAP_PROP_FPS, STREAM_FPS)
 
     while True:
         isTrue, frame = capture.read()
@@ -46,11 +53,18 @@ def GetCameraFrame():
         stream_no_gui.set_frame(frame)
 
         gui_frame = np.copy(frame)
+        gui_frame_x_dim = gui_frame.shape[0]
+        gui_frame_y_dim = gui_frame.shape[1]
 
         # logo
-        gui_frame = AddImage(gui_frame, "FRUCDHeader.png", 0, 0, 0.3)
+        gui_frame = AddImage(gui_frame, "static/FRUCDHeader.png", 0, 0, 0.3)
 
         # gauges
+        gui_frame = AddSpeedometer(gui_frame, int(gui_frame_y_dim/2), gui_frame_x_dim-10, 1)
+
+        #indicators
+        gui_frame = AddImage(gui_frame, "traction_control.png", 500, 500, 0.2)
+        gui_frame = AddImage(gui_frame, "speed_limiter.png", 100, 100, 0.2) # not working, use alpha channel
 
 
         cv.putText(gui_frame, str(time.time()), (255, 255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 0, 0), 2)
--- Telemetry Web + Camera GUI/telemetry_web_gui.py
+++ Telemetry Web + Camera GUI/telemetry_web_gui.py
@@ -1,12 +1,75 @@
-import justpy as jp
+from nicegui import app, ui
+import plotly.graph_objects as go
 
-def ShowStream():
-    wp = jp.WebPage()
+#FRUCD Brand 2.0 Colors
+FRUCD_DARK_BLUE = '#003a70'
 
-    # img = jp.Img(src='http://10.3.141.1:8080/camera', a=wp)
-    img = jp.Img(src='http://localhost:8080/gui', a=wp, style = "margin:auto; height:85%; width:85%")
+app.add_static_files('/static', 'static')
 
-    return wp
+def frucd_repeat_background():
+    ui.add_head_html("<style>body {background-image: url('/static/FRUCD_logo.png'); background-size: 5%;}</style>")
 
-#jp.justpy(ShowStream, host='10.3.141.1')
-jp.justpy(ShowStream)
+def main_navigation_menu():
+    with ui.button(icon='menu') as button:
+        button.classes(f'!bg-[{FRUCD_DARK_BLUE}]')
+        with ui.menu():
+            ui.menu_item('Home', on_click=lambda: ui.navigate.to('/', new_tab=False))
+            with ui.menu_item('Live Data', auto_close=False):
+                with ui.item_section().props('side'):
+                    ui.icon('keyboard_arrow_right')
+                with ui.menu().props("anchor='top end' self='top start' auto-close"):
+                    ui.menu_item('All Data', on_click=lambda: ui.navigate.to('/all_data', new_tab=False))
+                    ui.menu_item('Electrical')
+                    ui.menu_item('Cooling')
+                    ui.menu_item('Custom')
+            with ui.menu_item('Camera Feeds', auto_close=False):
+                with ui.item_section().props('side'):
+                    ui.icon('keyboard_arrow_right')
+                with ui.menu().props("anchor='top end' self='top start' auto-close"):
+                    ui.menu_item('GUI', on_click=lambda: ui.navigate.to('/camera_feed_gui', new_tab=False))
+                    ui.menu_item('No GUI', on_click=lambda: ui.navigate.to('/camera_feed_no_gui', new_tab=False))
+            ui.menu_item('Data Explorer')
+
+@ui.page('/camera_feed_gui')
+def camera_feed_gui():
+    frucd_repeat_background()
+    main_navigation_menu()
+
+    ui.image('http://localhost:8080/gui').style('margin:auto; height:85%; width:85%')
+
+@ui.page('/camera_feed_no_gui')
+def camera_feed_no_gui():
+    frucd_repeat_background()
+    main_navigation_menu()
+
+    ui.image('http://localhost:8080/no_gui').style('margin:auto; height:85%; width:85%')
+
+@ui.page('/all_data')
+def all_data():
+    frucd_repeat_background()
+    main_navigation_menu()
+
+    cols = 5
+    with ui.grid(columns=cols):
+        fig = go.Figure(go.Scatter(x=[1,2,3,4], y=[1,3,2,4]))
+        ui.plotly(fig)
+
+
+# TODO: ADD MAP, PLOTLY, LEFT/RIGHT SHELF, LOG VIEW
+
+frucd_repeat_background()
+
+with ui.card(align_items='center').classes('fixed-center'):
+    ui.image('/static/FRUCD_GD_White(1).png')
+    with ui.row():
+        with ui.dropdown_button('Live Data', auto_close=True).classes(f'!bg-[{FRUCD_DARK_BLUE}]'):
+            ui.item('All Data', on_click=lambda: ui.navigate.to('/all_data', new_tab=False))
+            ui.item('Electrical')
+            ui.item('Cooling')
+            ui.item('Custom')
+        with ui.dropdown_button('Camera Feeds', auto_close=True).classes(f'!bg-[{FRUCD_DARK_BLUE}]'):
+            ui.item('GUI', on_click=lambda: ui.navigate.to('/camera_feed_gui', new_tab=False))
+            ui.item('No GUI', on_click=lambda: ui.navigate.to('/camera_feed_no_gui', new_tab=False))
+        ui.button('Data Explorer').classes(f'!bg-[{FRUCD_DARK_BLUE}]')
+
+ui.run(port=8000, show=False)
