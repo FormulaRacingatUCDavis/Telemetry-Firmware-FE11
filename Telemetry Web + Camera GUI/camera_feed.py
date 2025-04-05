ec16681469f9720ef56031ae9c43ef390b28e3d0
maximum138 <maxwellyhchen@gmail.com>
2025-04-05 13:09:32 -0700

    polished camera_stream.py, made stream multiprocessing and a class, polished telemgui

--- Telemetry Web + Camera GUI/camera_feed.py
+++ Telemetry Web + Camera GUI/camera_feed.py
@@ -1,90 +1,126 @@
 import cv2 as cv
 import time
+import multiprocessing as mp
 import numpy as np
 from mjpeg_streamer import MjpegServer, Stream
 
-STREAM_QUALITY = 50
-STREAM_FPS = 60
-stream_gui = Stream("gui", quality = STREAM_QUALITY, fps = STREAM_FPS) # size = (1920, 1080) is optional
-stream_no_gui = Stream("no_gui", quality = STREAM_QUALITY, fps = STREAM_FPS)
+class CameraFeed:
+    def __init__(self, stream_ip="localhost", stream_port=8080, stream_quality=80, stream_fps=30):
+        # camera stream
+        self.STREAM_QUALITY = stream_quality
+        self.STREAM_FPS = stream_fps
+        self.STREAM_IP = stream_ip
+        self.STREAM_PORT = stream_port
+        self.is_recording = False
 
-# server = MjpegServer("10.3.141.1", 8080)
-server = MjpegServer("localhost", 8080)
-server.add_stream(stream_gui)
-server.add_stream(stream_no_gui)
-server.start()
+        STREAM_PROCESS = mp.Process(target=self.CameraMain)
+        STREAM_PROCESS.start()
 
-def AddImage(frame, image_src, x_pos, y_pos, scale = 1):
-    image = cv.imread(image_src)
+    def CurrentDateTime(self):
+        return time.strftime("%b_%d_%Y_%Hh_%Mm_%Ss", time.localtime())
 
-    image_height = int(image.shape[0] * scale)
-    image_width = int(image.shape[1] * scale)
+    def AddImage(self, frame, image_src, x_pos, y_pos, scale=1.0):
+        image = cv.imread(image_src)
 
-    image = cv.resize(image, dsize=(image_width, image_height), interpolation=cv.INTER_AREA)
+        image_height = int(image.shape[0] * scale)
+        image_width = int(image.shape[1] * scale)
 
-    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    ret, image_mask = cv.threshold(image_gray, 1, 255, type=cv.THRESH_BINARY)
+        image = cv.resize(image, dsize=(image_width, image_height), interpolation=cv.INTER_AREA)
 
-    image_loc = frame[x_pos:image_height+x_pos, y_pos:image_width+x_pos]
+        image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        ret, image_mask = cv.threshold(image_gray, 1, 255, type=cv.THRESH_BINARY)
 
-    image_loc[np.where(image_mask)] = 0
+        image_loc = frame[x_pos:image_height+x_pos, y_pos:image_width+x_pos]
 
-    image_loc += image
+        image_loc[np.where(image_mask)] = 0
 
-    return frame
+        image_loc += image
 
-def AddSpeedometer(frame, x_pos, y_pos, scale = 1):
-    normalized_value = ((frame[0,0,0] - 0) / (255 - 0)) * (360 - 180) + 180 # for speedometer testing
-    cv.ellipse(frame, (x_pos,y_pos), (100,100), 0, 180, normalized_value, (0,183,255), 10)
-    cv.putText(frame, str(frame[0,0,0]), (x_pos - 25,y_pos-25), cv.FONT_HERSHEY_DUPLEX, scale, (0,183,233), 2)
+        return frame
 
-    return frame
+    def AddSpeedometer(self, frame, x_pos, y_pos, scale=1):
+        normalized_value = ((frame[0,0,0] - 0) / (255 - 0)) * (360 - 180) + 180 # for speedometer testing
+        cv.ellipse(frame, (x_pos,y_pos), (100,100), 0, 180, normalized_value, (0,183,255), 10)
+        cv.putText(frame, str(frame[0,0,0]), (x_pos - 25, y_pos-25), cv.FONT_HERSHEY_DUPLEX, scale, (0,183,233), 2)
 
-def GetCameraFrame():
-    capture = cv.VideoCapture(0, cv.CAP_DSHOW)
-    # capture = cv.VideoCapture(0, cv.CAP_V4L2) # Solution: https://stackoverflow.com/questions/77190490/usb-camera-doesnt-work-with-opencv-and-raspberry-pi
-    capture.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
-    capture.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
-    capture.set(cv.CAP_PROP_FPS, STREAM_FPS)
+        return frame
 
-    while True:
-        isTrue, frame = capture.read()
+    def CameraMain(self):
+        # camera stream
+        stream_gui = Stream("gui", quality=self.STREAM_QUALITY, fps=self.STREAM_FPS)  # size = (1920, 1080) is optional
+        stream_no_gui = Stream("no_gui", quality=self.STREAM_QUALITY, fps=self.STREAM_FPS)
+        # self.server = MjpegServer("10.3.141.1", 8080)
+        server = MjpegServer(self.STREAM_IP, self.STREAM_PORT)
+        server.add_stream(stream_gui)
+        server.add_stream(stream_no_gui)
+        server.start()
 
-        stream_no_gui.set_frame(frame)
+        # camera capture
+        capture = cv.VideoCapture(0, cv.CAP_ANY)
+        # self.capture = cv.VideoCapture(0, cv.CAP_V4L2) # Solution: https://stackoverflow.com/questions/77190490/usb-camera-doesnt-work-with-opencv-and-raspberry-pi
+        capture.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+        capture.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+        capture.set(cv.CAP_PROP_FPS, self.STREAM_FPS)
+        stream_process = None  # separate stream process
 
-        gui_frame = np.copy(frame)
-        gui_frame_x_dim = gui_frame.shape[0]
-        gui_frame_y_dim = gui_frame.shape[1]
+        # stream recording
+        fourcc = cv.VideoWriter.fourcc('m', 'p', '4', 'v')
+        out = None
 
-        # logo
-        gui_frame = AddImage(gui_frame, "static/FRUCDHeader.png", 0, 0, 0.3)
+        def StartRecording():
+            nonlocal out
+            if not self.is_recording:
+                out = cv.VideoWriter(f"{self.CurrentDateTime()}_WebCam_Video.mp4", fourcc, 20, (int(capture.get(3)), int(capture.get(4))))
+                self.is_recording = True
 
-        # gauges
-        gui_frame = AddSpeedometer(gui_frame, int(gui_frame_y_dim/2), gui_frame_x_dim-10, 1)
+        def EndRecording():
+            if self.is_recording:
+                self.is_recording = False
+                out.release()
 
-        #indicators
-        #gui_frame = AddImage(gui_frame, "traction_control.png", 500, 500, 0.2)
-        #gui_frame = AddImage(gui_frame, "speed_limiter.png", 100, 100, 0.2) # not working, use alpha channel
+        def EndStream():
+            server.stop()
+            capture.release()
+            if self.is_recording:
+                out.release()
+
+            if __name__ == "__main__":
+                cv.destroyAllWindows()
+
+        while True:
+            is_true, frame = capture.read()
+
+            stream_no_gui.set_frame(frame)
+
+            gui_frame = np.copy(frame)
+            gui_frame_x_dim = gui_frame.shape[0]
+            gui_frame_y_dim = gui_frame.shape[1]
+
+            # logo
+            gui_frame = self.AddImage(gui_frame, "static/FRUCDHeader.png", 0, 0, 0.3)
+
+            # gauges
+            gui_frame = self.AddSpeedometer(gui_frame, int(gui_frame_y_dim/2), gui_frame_x_dim-10, 1)
+
+            #indicators
+            #gui_frame = AddImage(gui_frame, "traction_control.png", 500, 500, 0.2)
+            #gui_frame = AddImage(gui_frame, "speed_limiter.png", 100, 100, 0.2) # not working, use alpha channel
 
 
-        #cv.putText(gui_frame, str(time.time()), (255, 255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 0, 0), 2)
+            #cv.putText(gui_frame, str(time.time()), (255, 255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 0, 0), 2)
 
-        if __name__ == "__main__":
-            #cv.imshow('Camera', gui_frame)
+            stream_gui.set_frame(gui_frame)
 
-            if cv.waitKey(20) == ord(' '):
-                break
+            if self.is_recording:
+                out.write(gui_frame)
 
-        stream_gui.set_frame(gui_frame)
-
-    server.stop()
-    capture.release()
-
-    if __name__ == "__main__":
-        cv.destroyAllWindows()
+            if __name__ == "__main__":
+                # cv.imshow('Camera', frame)
+                if cv.waitKey(20) == ord(' '):
+                    break
 
 def main():
-   GetCameraFrame()
+    test = CameraFeed()
 
 if __name__ == "__main__":
     main()
--- Telemetry Web + Camera GUI/telemetry_web_gui.py
+++ Telemetry Web + Camera GUI/telemetry_web_gui.py
@@ -1,15 +1,23 @@
 import os
+import time
+import csv
+import asyncio
+import itertools
 from nicegui import app, ui
 import plotly.graph_objects as go
-import time
-import random
+from camera_feed import CameraFeed
 
-#FRUCD Brand 2.0 Colors
+# FRUCD Brand 2.0 Colors
 FRUCD_DARK_BLUE = '#003a70'
 
 CAMERA_STREAM_IP = 'localhost'
 
 app.add_static_files('/static', 'static')
+
+# camera steam initialization
+camera_stream = None
+if __name__ == "__main__":
+    camera_stream = CameraFeed()
 
 def frucd_repeat_background():
     ui.add_head_html("<style>body {background-image: url('/static/FRUCD_logo.png'); background-size: 5%;}</style>")
@@ -55,21 +63,22 @@ def all_data():
     main_navigation_menu()
 
     #ui.slider(min=1, max=5, value=3)
-    cols = 5
+    #cols = 5
     #with ui.grid(columns=cols):
     data = []
     fig = go.Figure()
     fig.add_scatter()
-    fig.update_layout(margin=dict(l=20, r=10, t=20, b=20))
+    fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
     plot = ui.plotly(fig)
 
     async def plotlyupdatetest():
-        data.append(time.time())
-        fig.data[0].y = data[:]
-        plot.update()
+        while True:
+            data.append(time.time())
+            fig.data[0].y = data[:]
+            plot.update()
+            await asyncio.sleep(0.1)
 
-    ui.button("Add Point", on_click=plotlyupdatetest)
-
+    asyncio.create_task(plotlyupdatetest())
 
 @ui.page('/data_explorer')
 async def data_explorer():
@@ -77,6 +86,8 @@ async def data_explorer():
 
     frucd_repeat_background()
     main_navigation_menu()
+
+    # TODO: ADD DATA REPLAY
 
     curr_directory = app.storage.tab.get('directory', 'static')
     def set_curr_directory(directory):
@@ -103,31 +114,51 @@ async def data_explorer():
             else:
                 ui.notify("No Files Selected", type='negative', close_button='Dismiss')
 
+        async def preview_selected_file():
+            selected_files = await explorer.get_selected_rows()
+            if selected_files:
+                if len(selected_files) != 1:
+                    ui.notify("Multiple File Preview Unsupported", type='warning', close_button='Dismiss')
+                else:
+                    # TODO: ADD DIFFERENT SCATTERS FOR EACH COLUMN
+                    with open(f"{curr_directory}/{selected_files[0]['file_name']}", newline='') as csv_file:
+                        reader = list(csv.reader(csv_file))
+                        data = list(map(list, itertools.zip_longest(*reader, fillvalue=None))) # transposes csv
+                        fig = go.Figure(go.Scatter(y=data[4][:]))
+                        fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
+                        plot = ui.plotly(fig)
+
+            else:
+                ui.notify("No File Selected", type='negative', close_button='Dismiss')
+
         with ui.row():
             with ui.dropdown_button('Directories', auto_close=True) as ddb:
                 ddb.classes(f'!bg-[{FRUCD_DARK_BLUE}]')
                 ui.item('Static', on_click=lambda: set_curr_directory('static'))
-                ui.item('CAN Data')
+                ui.item('CAN Data', on_click=lambda: set_curr_directory('csv_test_files'))
                 ui.item('Camera Stream Recordings')
 
             ui.button("Download Selected Files", on_click=download_selected_files).classes(f'!bg-[{FRUCD_DARK_BLUE}]')
+            ui.button("Preview Selected File", on_click=preview_selected_file).classes(f'!bg-[{FRUCD_DARK_BLUE}]')
 
 
 # TODO: ADD MAP, PLOTLY, LEFT/RIGHT SHELF, LOG VIEW
 
-frucd_repeat_background()
+@ui.page('/')
+def main_page():
+    frucd_repeat_background()
 
-with ui.card(align_items='center').classes('fixed-center'):
-    ui.image('/static/FRUCD_GD_White(1).png')
-    with ui.row():
-        with ui.dropdown_button('Live Data', auto_close=True).classes(f'!bg-[{FRUCD_DARK_BLUE}]'):
-            ui.item('All Data', on_click=lambda: ui.navigate.to('/all_data', new_tab=False))
-            ui.item('Electrical')
-            ui.item('Cooling')
-            ui.item('Custom')
-        with ui.dropdown_button('Camera Feeds', auto_close=True).classes(f'!bg-[{FRUCD_DARK_BLUE}]'):
-            ui.item('GUI', on_click=lambda: ui.navigate.to('/camera_feed_gui', new_tab=False))
-            ui.item('No GUI', on_click=lambda: ui.navigate.to('/camera_feed_no_gui', new_tab=False))
-        ui.button('Data Explorer', on_click=lambda: ui.navigate.to('/data_explorer', new_tab=False)).classes(f'!bg-[{FRUCD_DARK_BLUE}]')
+    with ui.card(align_items='center').classes('fixed-center'):
+        ui.image('/static/FRUCD_GD_White(1).png')
+        with ui.row():
+            with ui.dropdown_button('Live Data', auto_close=True).classes(f'!bg-[{FRUCD_DARK_BLUE}]'):
+                ui.item('All Data', on_click=lambda: ui.navigate.to('/all_data', new_tab=False))
+                ui.item('Electrical')
+                ui.item('Cooling')
+                ui.item('Custom')
+            with ui.dropdown_button('Camera Feeds', auto_close=True).classes(f'!bg-[{FRUCD_DARK_BLUE}]'):
+                ui.item('GUI', on_click=lambda: ui.navigate.to('/camera_feed_gui', new_tab=False))
+                ui.item('No GUI', on_click=lambda: ui.navigate.to('/camera_feed_no_gui', new_tab=False))
+            ui.button('Data Explorer', on_click=lambda: ui.navigate.to('/data_explorer', new_tab=False)).classes(f'!bg-[{FRUCD_DARK_BLUE}]')
 
 ui.run(port=8000, show=False)
